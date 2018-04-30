0a31cf2e09317feaa2500269be4311914290edfb
Hongyi Wang <hongyiwang@cs.wisc.edu>
2018-04-30 10:09:35 -0500

    modify script

--- de_facto/pytorch/experiment.sh
+++ de_facto/pytorch/experiment.sh
@@ -2,20 +2,20 @@
 echo "Running GPU experiment without fitting in memory ..."
 for method in logistic_regression least_square linear_svm
 do
-  for file in T_1000000 T_10000000 T_2000000 T_5000000
+  for m in 1000000 10000000 2000000 5000000
   do
-  	  echo "Running ${method} on ${file}"
+  	  echo "Running ${method} on T_${m}"
       python test.py \
 	    ${method} \
 	    --optim=SCD \
-	    --data-dir=./data/${file} \
-	    --num-tuples=10000000 \
+	    --data-dir=./data/T_${m} \
+	    --num-tuples=${m} \
 	    --num-feats=20 \
 	    --max-steps=50 \
 	    --lr=0.5 \
 	    --batch-size=1 \
 	    --load-in-memory= \
-	    --enable-gpu=True > GPU_${method}_${file}_without_fit
+	    --enable-gpu=True > GPU_${method}_T_${m}_without_fit
 	done
 done
 
@@ -23,20 +23,20 @@ done
 echo "Running GPU experiment fitting in memory ..."
 for method in logistic_regression least_square linear_svm
 do
-  for file in T_1000000 T_10000000 T_2000000 T_5000000
+  for m in 1000000 10000000 2000000 5000000
   do
-  	  echo "Running ${method} on ${file}"
+  	  echo "Running ${method} on T_${m}"
       python test.py \
 	    ${method} \
 	    --optim=SCD \
-	    --data-dir=./data/${file} \
-	    --num-tuples=10000000 \
+	    --data-dir=./data/T_${m} \
+	    --num-tuples=${m} \
 	    --num-feats=20 \
 	    --max-steps=50 \
 	    --lr=0.5 \
 	    --batch-size=1 \
 	    --load-in-memory=True \
-	    --enable-gpu=True > GPU_${method}_${file}_fitting
+	    --enable-gpu=True > GPU_${method}_T_${m}_fitting
 	done
 done
 
@@ -44,19 +44,19 @@ done
 echo "Running CPU experiment without fitting in memory ..."
 for method in logistic_regression least_square linear_svm
 do
-  for file in T_1000000 T_10000000 T_2000000 T_5000000
+  for m in 1000000 10000000 2000000 5000000
   do
-  	  echo "Running ${method} on ${file}"
+  	  echo "Running ${method} on T_${m}"
       python test.py \
 	    ${method} \
 	    --optim=SCD \
-	    --data-dir=./data/${file} \
-	    --num-tuples=10000000 \
+	    --data-dir=./data/T_${m} \
+	    --num-tuples=${m} \
 	    --num-feats=20 \
 	    --max-steps=50 \
 	    --lr=0.5 \
 	    --batch-size=1 \
 	    --load-in-memory= \
-	    --enable-gpu= > CPU_${method}_${file}_without_fit
+	    --enable-gpu= > CPU_${method}_T_${m}_without_fit
 	done
 done
